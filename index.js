385dcd347bb4cdac7396c460b155423fb5dd8b44
Damian Bravo <Dbravo1005@gmail.com>
2023-03-19 21:27:35 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+const employee = {
+    name: "Paul",
+    streetAddress: "23 Broadway"
+};
+
+// console.log(employee)
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    
+    // const employeeCopy = {...employee, [key] : value};
+    const employeeCopy = {...employee};
+    
+    employeeCopy[key] = value;
+    
+    return employeeCopy
+}
+
+// console.log(updateEmployeeWithKeyAndValue(employee, 'age', 30))
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value
+
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){ 
+    const newObj = {...employee}
+    delete newObj[key]
+   return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee [key]
+    return employee
+}
